8b6371ce115720f09269b3dea8742f56de1fb1e0
Yitzchak Lewkowicz <yitzchaklewkow@yahoo.com>
2022-04-28 10:21:35 -0600

    Created game object, basic mold level

---
+++ src/00-objectball.js
@@ -0,0 +1,68 @@
+function gameObject() {
+    const game = {
+        home: {
+            teamName: 'Brooklyn Nets',
+            colors: ['Black', 'White']
+            players: { 
+                    'Alan Anderson': {
+                        number: '',
+                        shoe: '',
+                        points: '',
+                        rebounds: '',
+                        assists: '',
+                        steals: '',
+                        blocks: '',
+                        slamDunks: ''
+                    },
+                    'Alan Anderson': {
+                        number: '',
+                        shoe: '',
+                        points: '',
+                        rebounds: '',
+                        assists: '',
+                        steals: '',
+                        blocks: '',
+                        slamDunks: ''
+                    },
+                    'Alan Anderson': {
+                        number: '',
+                        shoe: '',
+                        points: '',
+                        rebounds: '',
+                        assists: '',
+                        steals: '',
+                        blocks: '',
+                        slamDunks: ''
+                    },
+                    'Alan Anderson': {
+                        number: '',
+                        shoe: '',
+                        points: '',
+                        rebounds: '',
+                        assists: '',
+                        steals: '',
+                        blocks: '',
+                        slamDunks: ''
+                    },
+                    'Alan Anderson': {
+                        number: '',
+                        shoe: '',
+                        points: '',
+                        rebounds: '',
+                        assists: '',
+                        steals: '',
+                        blocks: '',
+                        slamDunks: ''
+                    }                
+
+            }
+        },
+        
+        away: {
+            teamName: 'Charlotte Hornets',
+            colors: ['Turquoise', 'Purple'],
+            players: ''
+        }
+
+    }
+}
